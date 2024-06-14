e0d41ad897965b34a6d5c97036bed9a9ceb8897b
mateuslopessousa <mateuslopessousa>
2024-06-14 18:22:55 +0000

    teste

--- Formula1_Exemplo/Formula1-Project-Solutions/Section-07-08/set-up/3.access_adls_using_service_principal.py
+++ Formula1_Exemplo/Formula1-Project-Solutions/Section-07-08/set-up/3.access_adls_using_service_principal.py
@@ -9,25 +9,25 @@
 
 # COMMAND ----------
 
-client_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-client-id')
-tenant_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-tenant-id')
-client_secret = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-client-secret')
+client_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'client-id')
+tenant_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'tenant-id')
+client_secret = dbutils.secrets.get(scope = 'formula1-scope', key = 'client-secret')
 
 # COMMAND ----------
 
-spark.conf.set("fs.azure.account.auth.type.formula1dl.dfs.core.windows.net", "OAuth")
-spark.conf.set("fs.azure.account.oauth.provider.type.formula1dl.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set("fs.azure.account.oauth2.client.id.formula1dl.dfs.core.windows.net", client_id)
-spark.conf.set("fs.azure.account.oauth2.client.secret.formula1dl.dfs.core.windows.net", client_secret)
-spark.conf.set("fs.azure.account.oauth2.client.endpoint.formula1dl.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
+spark.conf.set("fs.azure.account.auth.type.formula1dlmls.dfs.core.windows.net", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type.formula1dlmls.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id.formula1dlmls.dfs.core.windows.net", client_id)
+spark.conf.set("fs.azure.account.oauth2.client.secret.formula1dlmls.dfs.core.windows.net", client_secret)
+spark.conf.set("fs.azure.account.oauth2.client.endpoint.formula1dlmls.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
 
 # COMMAND ----------
 
-display(dbutils.fs.ls("abfss://demo@formula1dl.dfs.core.windows.net"))
+display(dbutils.fs.ls("abfss://demo@formula1dlmls.dfs.core.windows.net"))
 
 # COMMAND ----------
 
-display(spark.read.csv("abfss://demo@formula1dl.dfs.core.windows.net/circuits.csv"))
+display(spark.read.csv("abfss://demo@formula1dlmls.dfs.core.windows.net/circuits.csv"))
 
 # COMMAND ----------
 
